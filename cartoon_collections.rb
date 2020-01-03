aa04c91563598ef6e266e079714163e3f8592f23
Lugh Wilson <lughjw@gmail.com>
2020-01-03 18:47:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,9 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index{|dwarf,i| puts "#{i+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  
+  
 end
 
 def long_planeteer_calls# code an argument here
