74331d015ebe3febfbff96b3a901fb1a5d78de25
Lugh Wilson <lughjw@gmail.com>
2020-01-03 18:57:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(array)
   array.any?{|item| item.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(potential_cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  potential_cheeses.contain?(cheese_types)
 end
